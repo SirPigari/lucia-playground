e3ae6e24ac766ab9b10d01078555c066509d3e63
SirPigari <leonardmarkovic015@gmail.com>
2025-09-14 22:11:07 +0200

    fixed next cache

--- app/next.config.js
+++ app/next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+
 module.exports = {
     reactStrictMode: true,
     output: 'export',
-    images: {
-        unoptimized: true,
-    },
+    images: { unoptimized: true },
+    basePath: isGitHubPages ? '/lucia-playground' : '',
+    assetPrefix: isGitHubPages ? '/lucia-playground/' : '',
 };
